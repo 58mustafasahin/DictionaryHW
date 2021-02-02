62923482b77d51a20fd550ab6b9e5d03090f3da4
58mustafasahin <75518877+58mustafasahin@users.noreply.github.com>
2021-02-02 21:06:45 +0300

    Add project files.

---
+++ DictionaryHW/MyDictionary.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DictionaryHW
+{
+    class MyDictionary<P, I>
+    {
+        P[] _arrayKey;
+        P[] _tempArrayKey;
+        I[] _arrayValue;
+        I[] _tempArrayValue;
+
+        public MyDictionary()
+        {
+            _arrayKey = new P[0];
+            _arrayValue = new I[0];
+        }
+
+        public void Add(P key, I value)
+        {
+            _tempArrayKey = _arrayKey;
+            _tempArrayValue = _arrayValue;
+
+            _arrayKey = new P[_arrayKey.Length + 1];
+            _arrayValue = new I[_arrayValue.Length + 1];
+
+            for (int i = 0; i < _tempArrayKey.Length; i++)
+            {
+                _arrayKey[i] = _tempArrayKey[i];
+
+            }
+            for (int i = 0; i < _tempArrayValue.Length; i++)
+            {
+                _arrayValue[i] = _tempArrayValue[i];
+            }
+
+            _arrayKey[_arrayKey.Length - 1] = key;
+            _arrayValue[_arrayValue.Length - 1] = value;
+
+            for (int i = 0; i < _arrayKey.Length; i++)
+            {
+                Console.WriteLine("Plaka : " + _arrayKey[i] + " Şehir Adı : " + _arrayValue[i]);
+            }
+            Console.WriteLine("");
+        }
+    }
+}
---
+++ DictionaryHW/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DictionaryHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> sehirler = new MyDictionary<int, string>();
+            sehirler.Add(24, "Erzincan");
+            sehirler.Add(35, "İzmir");
+            sehirler.Add(58, "Sivas");
+            sehirler.Add(26, "Eskişehir");
+            sehirler.Add(16, "Bursa");
+
+        }
+    }
+}
